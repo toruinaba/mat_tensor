3a09dc29fe6bb492ae33f3c125286f6c27580440
toruinaba <inaba.tooru@takenaka.co.jp>
2025-01-30 17:52:45 +0900

    Modify/Solver

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-TOL = 1.0e-06
-STEP = 125
 
 Ni = np.array([
     [1, 1, 1, 1, 1, 1],
@@ -50,7 +48,6 @@ def kron_delta(A, B):
                 mat[i_m, i_n] = 1
     return mat
 
-
 I = kron_delta(Ni, Nk) * kron_delta(Nj, Nl)
 I_t = kron_delta(Ni, Nl) * kron_delta(Nj, Nk)
 IxI = kron_delta(Ni, Nj) * kron_delta(Nk, Nl)
@@ -59,11 +56,16 @@ i = np.array([1, 1, 1, 0, 0, 0])
 
 Id = Is - 1 / 3 * IxI
 
+TOL = 1.0e-06
+STEP = 600
+RM_I = 10
+NW_I = 100
+
 E = 205000.0
 n = 0.3
 sig_y = 235
-H = 20000
-b = 30.0
+Q = 300.0
+b = 150.0
 
 G = E / (2 * (1 + n))
 A = 1
@@ -76,151 +78,117 @@ eps = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 eps_e = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 eps_p = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 eff_eps_p = 0.0
+r = 0.0
 
-goal_sig = np.array([400.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-
-def calc_yield_stress(eff_eps_p):
-    return sig_y + H *eff_eps_p
+goal_sig = np.array([800.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
 
-def calc_plastic_mod(eff_eps_p):
-    return H
+sig_hist = []
+eps_hist = []
+eps_e_hist = []
+eps_p_hist = []
+eff_eps_p_hist = []
+r_hist = []
+mises_hist = []
 
-def return_mapping(eps_e_d_tri, eff_eps_p):
-    sig_d_tri = 2 * G * eps_e_d_tri
-    q_tri = np.sqrt(3 / 2 * sig_d_tri @ sig_d_tri)
-    if q_tri == 0.0:
-        n_vector = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    else:
-        n_vector = sig_d_tri / np.sqrt(sig_d_tri @ sig_d_tri)
-    
-    y = calc_yield_stress(eff_eps_p)
-    phi = q_tri - y
-    hd = calc_plastic_mod(eff_eps_p)
-    print(f"Yield stress: {y}")
-    print(f"Phi: {phi}")
-    
-    del_gam = 0.0
-    if phi >= 0.0001:
-        for itr in range(10):
-            diff = q_tri - 3 * G * del_gam - y
-            print(f"Return map difference: {diff}")
-            if abs(diff) < TOL * y:
-                print("Return map converged")
-                break
-            del_gam += diff / (3 * G + hd)
-            y = calc_yield_stress(eff_eps_p + del_gam)
-    print(f"Q_tri: {q_tri}")
-    eps_e_d = (1 -  3 * del_gam * G / q_tri) * eps_e_d_tri
-    sig_d = 2 * G * eps_e_d
-    print(f"Corrected sig_d: {sig_d}")
-    mises = np.sqrt(3 / 2 * sig_d @ sig_d)
-    print(f"Corrected Mises: {mises}")
-    print(f"Current Y: {calc_yield_stress(eff_eps_p + del_gam)}")
-    return q_tri, del_gam, n_vector, hd
+for i in range(STEP):
+    print("="*80)
+    print(f"Increment {i}")
+    del_eps = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    sig_ip1 = sig + np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-def calc_Dep(eps, eps_p, eff_eps_p):
-    eps_e_tri = eps - eps_p
+    Dep = De
+    f_sig = sig_ip1 - sig
+    del_eps = np.linalg.inv(Dep) @ f_sig
+    print(f"Initial dela eps: {del_eps}")
 
-    eps_e_d_tri = Id @ eps_e_tri
-    q_tri, del_gam, n_vector, hd = return_mapping(eps_e_d_tri, eff_eps_p)
-    NxN = np.matrix(n_vector).transpose() @ np.matrix(n_vector)
-
-    if q_tri == 0.0:
-        Dep = 2 * G * Id + K * IxI
-    else:
-        sig_e_d = 2 * G * (1 - 3 * del_gam * G / q_tri) * eps_e_d_tri
-        sig_e_v = K * IxI @ eps
-        print(f"Sig_e_d: {sig_e_d}")
-        print(f"Sig_e_v: {sig_e_v}")
-        print(f"Mises: {np.sqrt(3 / 2 * sig_e_d @ sig_e_d)}")
-        Dep = (
-            2 * G * (1 - 3 * del_gam * G / q_tri) * Id +
-            6 * G**2 * (del_gam / q_tri - 1 / (3 * G + hd)) * NxN * (1.0 if del_gam != 0.0 else 0.0) +
-            K * IxI
-        )
-    return np.array(Dep), del_gam, n_vector, q_tri
-
-miseses = []
-eff_eps_ps = []
-
-
-pl_flag = False
-
-for inc in range(STEP):
-    print("-"*30 + f"INCREMENT {inc}" + "-"*30)
-    del_sig = np.array([2.0, 0, 0, 0, 0, 0])
-    sig_tri = sig + del_sig
-    print(f"Goal Sigma: {sig_tri}")
-    print(f"Current Eps: {eps}")
-    del_eps = np.array([0.0]*6)
-    d_del_eps = np.array([0.0]*6)
-    eps_tri = eps + del_eps
-    del_sig_ep = np.array([0.0]*6)
-    del_eps_p = np.array([0.0]*6)
-    del_eff_eps_p = 0.0
-
-    Dep, del_gam, n_vector, q_tri = calc_Dep(eps, eps_p, eff_eps_p)
-    del_sig_ep = Dep @ d_del_eps
-
-    for itr in range(10):
-        print(f"Iteration {itr}")
-        print(f"Eps_tri on Iter.{itr}: {eps_tri}")
-        print(f"Delta gamma: {del_gam}")
-        print(f"N vector: {n_vector}")
-        print(f"Del sig ep: {del_sig_ep}")
-        corrected_sig = sig + del_sig_ep
-        sig_diff = sig_tri - corrected_sig
-        print(f"Sigma difference: {sig_diff}")
-        diff_norm = np.sqrt(sig_diff @ sig_diff)
-        if diff_norm  < 0.0001:
-            print("ResForce Converged")
-            break
-        if itr == 9:
-            raise ValueError("NotConverged")
-        d_del_eps = np.linalg.inv(Dep) @ sig_diff
-        del_eps += d_del_eps
-        del_eps_p += 3 / 2 * del_gam * n_vector
-        del_eff_eps_p += del_gam
-        del_sig_ep += Dep @ d_del_eps
-
+    for itr in range(NW_I):
+        print("-"*80)
+        print(f"Iteration: {itr+1}")
         eps_tri = eps + del_eps
-        eps_p_tri = eps_p + del_eps_p
-        eff_eps_p_tri = eff_eps_p + del_eff_eps_p
-        Dep, del_gam, n_vector, q_tri = calc_Dep(eps_tri, eps_p_tri, eff_eps_p_tri)
-        print(f"Delta Eps tri on Iter.{itr}: {d_del_eps}")
-        print(f"Updated Eps Tri on Iter.{itr}: {eps_tri}")
-        print(f"Updated Eps_p Tri on Iter.{itr}: {eps_p_tri}")
-        print(f"Updated Eff_eps_p Tri on Iter.{itr}: {eff_eps_p_tri}")
+        eps_e_tri = eps_tri - eps_p
+        eps_e_d_tri = Id @ eps_e_tri
+        sig_tri = De @ eps_e_tri
+        sig_d_tri = Id @ sig_tri
+        sig_v_tri = 1 / 3 * IxI @ sig_tri
 
-        
-    
-    eps += del_eps
-    sig = sig_tri
-    print(f"Updated Eps: {eps}")
-    eps_p += del_eps_p
-    eps_e = eps - eps_p
-    eps_e_d = Id @ eps_e
-    sig_d = 2 * G * eps_e_d
-    eff_eps_p += del_eff_eps_p 
-    print(f"Updated Eps_e: {eps_e}")
-    print(f"Updated Eps_p: {eps_p}")
-    print(f"Updated eff_eps_p: {eff_eps_p}")
-    mises = np.sqrt(3 / 2 * sig_d @ sig_d)
-    print(f"Mises: {mises}")
-    print(f"Current Sig_y: {calc_yield_stress(eff_eps_p)}")
-    if eff_eps_p != 0.0:
-        print(f"Hardening mod: {(mises - miseses[-1])/(eff_eps_p - eff_eps_ps[-1])}")
-    eff_eps_ps.append(eff_eps_p)
-    miseses.append(mises)
+        q_tri = np.sqrt(3/2 * sig_d_tri @ sig_d_tri)
+        n_bar = sig_d_tri / q_tri
+        del_gam = 0.0
+        theta = 1.0
 
+        f_ip1 = sig_y + r - q_tri # applied del_gam = 0, theta = 0
+        f_ip1_prime = 3 * G - b * (r + Q) # applied del_gam = 0, theta = 0
+        if f_ip1 < 0.0:
+            print("Plastic behavior")
+            for inew in range(RM_I):
+                print(f"Return map iteration {inew+1}")
+                print(f_ip1)
+                d_del_gam = f_ip1 / f_ip1_prime
+                del_gam -= d_del_gam
+                theta = 1 / (1 + b * del_gam)
+                f_ip1 = sig_y + theta * (r + b * Q * del_gam) + 3 * G * del_gam - q_tri
+                f_ip1_prime = 3 * G - b * theta**2 * (r + b * Q * del_gam) + theta * b * Q
+                if abs(f_ip1) < TOL:
+                    if del_gam < 0.0:
+                        raise ValueError("Delta gamma is negative value.")
+                    print(f"Return map converged itr.{inew+1}")
+                    print(f"Delta gamma: {del_gam}")
+                    break
+                if inew == RM_I - 1:
+                    raise ValueError("Return map isn't converged")
+            delta_eps_p = np.sqrt(3 / 2) * del_gam * n_bar
+            eps_p_ip1 = eps_p + delta_eps_p
+            eps_e_ip1 = eps_tri - eps_p_ip1
+            eps_e_d_ip1 = Id @ eps_e_ip1
+            eps_ip1 = eps_tri
+            r_ip1 = theta * (r + b * Q * del_gam)
+            eff_eps_p_ip1 = eff_eps_p + del_gam
+            sig_r = 2 * G * eps_e_d_ip1 + sig_v_tri
+        else:
+            print("Elastic behavior")
+            eps_ip1 = eps_tri
+            eps_e_ip1 = eps_e_tri
+            eps_p_ip1 = eps_tri - eps_e_tri
+            r_ip1 = r
+            eff_eps_p_ip1 = eff_eps_p
+            sig_r = De @ eps_e_ip1
+
+        sig_diff = sig_ip1 - sig_r
+        print(f"Corrected sig: {sig_r}")
+        print(f"Difference norm: {np.sqrt(sig_diff @ sig_diff)}")
+        if np.sqrt(3 / 2 * sig_diff @ sig_diff) / (sig_y + r) < TOL:
+            sig = sig_ip1
+            sig_hist.append(sig)
+            eps_e = eps_e_ip1
+            eps_e_hist.append(eps_e)
+            eps_p = eps_p_ip1
+            eps_p_hist.append(eps_p)
+            eps = eps_ip1
+            eps_hist.append(eps)
+            r = r_ip1
+            r_hist.append(r)
+            eff_eps_p = eff_eps_p_ip1
+            eff_eps_p_hist.append(eff_eps_p)
+            sig_d = Id @ sig
+            mises = np.sqrt(3/2 * sig_d @ sig_d)
+            mises_hist.append(mises)
+            print(f"Iteration converged: itr.{itr+1}")
+            print(f"Mises: {mises}")
+            break
+        else:
+            print(f"itr.{itr+1}")
+            Dep = De - 6 * G**2 * del_gam / q_tri * Id + 6 * G**2 * (del_gam / q_tri - 1 / f_ip1_prime) * np.outer(n_bar, n_bar)
+            d_del_eps = np.linalg.inv(Dep) @ sig_diff
+            del_eps += d_del_eps
+            print(f"Updated delta eps: {del_eps}")
+        if itr == NW_I - 1:
+            raise ValueError(f"Not converged this iteration.")
 
 from matplotlib import pyplot as plt
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(eff_eps_ps, miseses)
-ax.plot([0, 0.002], [sig_y, sig_y])
+e11 = [x[0] for x in eps_p_hist]
+s11 = [x[0] for x in sig_hist]
+plt.plot(e11, s11)
 plt.show()
-
-
