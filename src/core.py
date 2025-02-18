2b9bafaf912772db0a556d8ff07f75188ced54c9
inb_t.app_mng@icloud.com <inb_t.app_mng@icloud.com>
2025-02-19 08:46:11 +0900

    Modify/Move integrate function to material

--- src/core.py
+++ src/core.py
@@ -5,7 +5,6 @@ from src.material import Material_expression_base
 
 class Calculator3D:
     TOL = 1.0e-06
-    RM_I = 10
     NW_I = 100
 
     def __init__(self, material: Material_expression_base, goal_sig: np.ndarray, step: int):
@@ -28,48 +27,6 @@ class Calculator3D:
         self.sig = np.array([0.0]*6)
         self.output.initialize()
 
-    def return_mapping(self, sig_d_tri):
-        for itr in range(self.NW_I):
-            print("-"*80)
-            del_gam = 0.0
-            f_ip1 = self.material.calc_f_ip1(sig_d_tri, del_gam)
-            f_ip1_prime = self.material.calc_f_ip1_prime(sig_d_tri, del_gam)
-            if f_ip1 < 0.0:
-                print("Plastic behavior")
-                for inew in range(self.RM_I):
-                    print(f"Newton iteration {inew+1}")
-                    d_del_gam = f_ip1 / f_ip1_prime
-                    del_gam -= d_del_gam
-                    f_ip1 = self.material.calc_f_ip1(sig_d_tri, del_gam)
-                    f_ip1_prime = self.material.calc_f_ip1_prime(sig_d_tri, del_gam)
-                    if abs(f_ip1) < self.TOL:
-                        if del_gam < 0.0:
-                            raise ValueError("Delta gamma is negative value.")
-                        print(f"Return map converged itr.{inew+1}")
-                        print(f"Delta gamma: {del_gam}")
-                        break
-                    if inew == self.RM_I - 1:
-                        raise ValueError("Return map isn't converged")
-            else:
-                print("Elastic behavior")
-            q_tri, n_bar = self.material.calc_tri(sig_d_tri, del_gam)
-            return q_tri, del_gam, n_bar
-
-    def integrate_stress(self, del_eps):
-        eps_tri = self.eps + del_eps
-        eps_e_tri = eps_tri - self.material.eps_p
-        sig_tri = self.material.elastic.De @ eps_e_tri
-        sig_d_tri = Id_s @ sig_tri
-
-        q_tri, del_gam, n_bar = self.return_mapping(sig_d_tri)
-        print(f"F: {self.material.calc_f_ip1(sig_d_tri, del_gam)}")
-        self.material.update_i(del_gam, n_bar)
-        eps_e_i = eps_tri - self.material.eps_p_i
-        sig_e = self.material.elastic.De @ eps_e_i
-        sig = sig_tri - 2 * self.material.elastic.G * del_gam * n_bar * np.sqrt(3 / 2)
-        Dep = self.material.calc_Dep(sig_d_tri, del_gam)
-        return sig, Dep
-
     def calc_increment(self, goal):
         f_sig = goal - self.sig
         del_eps = np.linalg.inv(self.material.elastic.De) @ f_sig
@@ -79,7 +36,7 @@ class Calculator3D:
         for itr in range(self.NW_I):
             print("-"*80)
             print(f"Iteration: {itr+1}")
-            sig_i, Dep = self.integrate_stress(del_eps)
+            sig_i, Dep = self.material.integrate_stress(self.eps + del_eps)
             print(f"Corrected sig: {sig_i}")
             sig_diff = goal - sig_i
             sig_diff_norm = self.calc_stress_norm(sig_diff)
